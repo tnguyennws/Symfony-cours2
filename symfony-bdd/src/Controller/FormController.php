b132c8d6a15cabb155b3fc054265fdf0dc601c94
Timothée Nguyen <tnguyen@normandiewebschool.fr>
2021-05-30 21:34:35 +0200

    Ajout BDD

--- symfony-bdd/src/Controller/FormController.php
+++ symfony-bdd/src/Controller/FormController.php
@@ -6,10 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\AppelFormType;
+use App\Form\Repository\AppelRepository;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Appel;
 
 class FormController extends AbstractController
 {
+
+    /**
+     * @Route ("/appel")
+     */
+    public function AppeList()
+    {
+        $appel = $this->getDoctrine()
+            ->getRepository(Appel::class)
+            ->findAll();
+
+        return ($this->render('form/appel.html.twig',
+        [
+            'appel' => $appel
+        ]));
+    } 
+
     /**
      * @Route("/form", name="formPost", methods={"POST"})
      */
